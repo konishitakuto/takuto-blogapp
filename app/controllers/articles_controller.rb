daef611a2c5f96b7dfada3fa869ec8cfa29a1703
小西 拓斗 <>
2020-08-22 11:54:25 +0900

    formを部分テンプレートに変更しました

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update]
+
   def index
-@articles = Article.all
+    @articles = Article.all
   end
 
   def show
-   @article = Article.find(params[:id])
   end
 
   def new
@@ -22,11 +24,9 @@ class ArticlesController < ApplicationController
   end
 
     def edit
-      @article = Article.find(params[:id])
     end
 
     def update
-      @article = Article.find(params[:id])
       if @article.update(article_params)
         redirect_to article_path(@article), notice: '更新できました'
       else
@@ -42,9 +42,13 @@ class ArticlesController < ApplicationController
       redirect_to root_path, notice: '削除に成功しました'
     end 
 
-
   private
   def article_params
     params.require(:article).permit(:title, :content)
   end
+
+  def set_article
+    @article = Article.find(params[:id])
+   end
+
 end
