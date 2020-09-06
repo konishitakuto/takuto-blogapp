e38aa0b56c5dbbe8ee8c2916bd4c7afa0911b7c0
小西 拓斗 <>
2020-09-06 21:54:06 +0900

    Ajaxでコメント投稿機能作成

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -12,40 +12,82 @@ const handleHeartDisplay = (hasLiked) => {
   }
 }
 
+const handleCommentForm = () => {
+  $('.show-comment-form').on('click', () => {
+    $('.show-comment-form').addClass('hidden')
+    $('.comment-text-area').removeClass('hidden')
+  })
+}
+
+const appendNewComment = (comment) => {
+  $('.comments-container').append(
+    `<div class="article_comment"><p>${comment.content}</p></div>`
+  )
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const dataset = $('#article-show').data()
   const articleId = dataset.articleId
+
+  axios.get(`/articles/${articleId}/comments`)
+  .then((response) => {
+    const comments = response.data
+    comments.forEach((comment) => {
+      appendNewComment(comment)
+    })
+  })
+
+
   axios.get(`/articles/${articleId}/like`)
     .then((response) => {
       const hasLiked = response.data.hasLiked
       handleHeartDisplay(hasLiked)
-    })
+  })
     
-    $('.inactive-heart').on('click', () => {
-      axios.post(`/articles/${articleId}/like`)
-        .then((response) => {
-          if (response.data.status === 'ok') {
-            $('.active-heart').removeClass('hidden')
-            $('.inactive-heart').addClass('hidden')
-          }
-        })
-        .catch((e) => {
-          window.alert('Error')
-          console.log(e)
-        })
-    })
+  $('.inactive-heart').on('click', () => {
+    axios.post(`/articles/${articleId}/like`)
+      .then((response) => {
+        if (response.data.status === 'ok') {
+          $('.active-heart').removeClass('hidden')
+          $('.inactive-heart').addClass('hidden')
+        }
+      })
+      .catch((e) => {
+        window.alert('Error')
+        console.log(e)
+      })
+  })
 
-    $('.active-heart').on('click', () => {
-      axios.delete(`/articles/${articleId}/like`)
-        .then((response) => {
-          if (response.data.status === 'ok') {
-            $('.active-heart').addClass('hidden')
-            $('.inactive-heart').removeClass('hidden')
-          }
+  handleCommentForm()
+
+  $('.add-comment-button').on('click', () => {
+    const content = $('#comment_content').val()
+    if (!content) {
+      window.alert('コメントを入力してください')
+    }else{
+      axios.post(`/articles/${articleId}/comments`, {
+        comment: {content: content}
+      })
+        .then((res) => {
+          const comment = res.data
+          appendNewComment(comment)
+          $('#comment_content').val('')
         })
-        .catch((e) => {
-          window.alert('Error')
-          console.log(e)
-        })
-    })
+    }
+  })
+
+  $('.active-heart').on('click', () => {
+    axios.delete(`/articles/${articleId}/like`)
+      .then((response) => {
+        if (response.data.status === 'ok') {
+          $('.active-heart').addClass('hidden')
+           $('.inactive-heart').removeClass('hidden')
+        }
+      })
+      .catch((e) => {
+        window.alert('Error')
+        console.log(e)
+      })
+  })
 })
